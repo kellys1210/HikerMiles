c6c2ac4fc537bff216ade765d6ac39c917e17e47
Eric Peters <ericrpeters@protonmail.com>
2025-02-07 14:26:48 -0800

    adding data manipulation SQL for patrons

---
+++ SQL/data_manipulation.sql
@@ -0,0 +1,30 @@
+-- Patron Directory
+
+-- Read
+Select patron_id, name, date_of_birth, address
+From Patrons;
+
+-- Create
+Insert Into Patrons (name, date_of_birth, address)
+    Values (:name, :date_of_birth, :address);
+
+-- Update
+Set name = :name, date_of_birth = :date_of_birth, 
+    address = :address
+    Where id = :patron_id;
+
+-- Delete
+Delete from Patrons
+    Where id = :patron_id;
+
+-- Rewards Directory
+
+-- Park Directory
+
+-- Trail Directory
+
+-- Brain Implant Directory
+
+-- Patron Park Directory
+
+-- Patron Trail Directory
