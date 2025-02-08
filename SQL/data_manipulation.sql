da7aa8e0cc8f9c246fdda4ce7d49abd2aa5fa18c
AustinOregonState <146279644+AustinJHolcomb@users.noreply.github.com>
2025-02-07 19:04:36 -0600

    Updated brain_implants, parks, patrons, rewards_points, trails to have update and delete fields. These files also had their required fields updated to match our database schema diagram.

--- SQL/data_manipulation.sql
+++ SQL/data_manipulation.sql
@@ -1,146 +1,168 @@
--- Patron Directory--------------------------------------------
+--------------- Patron Directory ---------------
+
 -- Read
-Select patron_id, name, date_of_birth, address
-From Patrons;
+SELECT patron_id, name, date_of_birth, address
+FROM Patrons;
 
 -- Create
-Insert Into Patrons (name, date_of_birth, address)
-    Values (:name, :date_of_birth, :address);
+INSERT INTO Patrons (name, date_of_birth, address)
+VALUES (:name, :date_of_birth, :address);
 
 -- Update
-Update Patrons
-Set name = :name, date_of_birth = :date_of_birth, 
+UPDATE Patrons
+SET name = :name, 
+    date_of_birth = :date_of_birth, 
     address = :address
-    Where patron_id = :patron_id;
+WHERE patron_id = :patron_id;
 
 -- Delete
-Delete from Patrons
-    Where patron_id = :patron_id;
+DELETE FROM Patrons
+WHERE patron_id = :patron_id;
 
--- Rewards Directory-------------------------------------------
+--------------- Rewards Directory ---------------
+
 -- Read
-Select reward_id, patron_id, distance, reward
-From RewardPoints;
+SELECT reward_id, patron_id, distance, reward
+FROM RewardPoints;
 
 -- Create
-Insert Into RewardPoints 
-    (name, distance, reward)
-    Values ((SELECT name FROM Patrons WHERE name = :name), :distance, :reward);
+INSERT INTO RewardPoints (name, distance, reward)
+VALUES (
+    (SELECT name FROM Patrons WHERE name = :name), 
+    :distance, 
+    :reward
+);
 
 -- Update
-Update RewardPoints
-Set name = (SELECT name FROM Patrons WHERE name = :name),
-    distance = :distance, reward = :reward
-    Where reward_id = :reward_id_id;
+UPDATE RewardPoints
+SET name = (SELECT name FROM Patrons WHERE name = :name), 
+    distance = :distance, 
+    reward = :reward
+WHERE reward_id = :reward_id;
 
 -- Delete
-Delete from RewardPoints
-    Where reward_id = :reward_id;
+DELETE FROM RewardPoints
+WHERE reward_id = :reward_id;
 
--- Park Directory----------------------------------------------
+--------------- Park Directory ---------------
+
 -- Read
-Select park_id, name, state, county, has_ranger_station
-From Parks;
+SELECT park_id, name, state, county, has_ranger_station
+FROM Parks;
 
 -- Create
-Insert Into Parks (name, state, county, has_ranger_station)
-    Values (:name, :state, :county, :has_ranger_station);
+INSERT INTO Parks (name, state, county, has_ranger_station)
+VALUES (:name, :state, :county, :has_ranger_station);
 
 -- Update
-Update Parks
-Set name = :name, state = :state, 
-    county = :county, has_ranger_station = :has_ranger_station
-    Where park_id = :park_id;
+UPDATE Parks
+SET name = :name, 
+    state = :state, 
+    county = :county, 
+    has_ranger_station = :has_ranger_station
+WHERE park_id = :park_id;
 
 -- Delete
-Delete from Parks
-    Where park_id = :park_id;
+DELETE FROM Parks
+WHERE park_id = :park_id;
 
--- Trail Directory---------------------------------------------
+--------------- Trail Directory ---------------
+
 -- Read
-Select trail_id, park_id, name, trail_head_coord, length
-From Trails;
+SELECT trail_id, park_id, name, trail_head_coord, length
+FROM Trails;
 
 -- Create
-Insert Into Trails 
-    (name, trail_head_coord, length)
-    Values ((SELECT name FROM Parks WHERE name = :name),
-     :trail_head_coord, :length);
+INSERT INTO Trails (name, trail_head_coord, length)
+VALUES (
+    (SELECT name FROM Parks WHERE name = :name), 
+    :trail_head_coord, 
+    :length
+);
 
 -- Update
-Update Trails
-Set name = (SELECT name FROM Parks WHERE name = :name),
-    trail_head_coord = :trail_head_coord, length = :length
-    Where trail_id = :trail_id;
+UPDATE Trails
+SET name = (SELECT name FROM Parks WHERE name = :name), 
+    trail_head_coord = :trail_head_coord, 
+    length = :length
+WHERE trail_id = :trail_id;
 
 -- Delete
-Delete from Trails
-    Where trail_id = :trail_id;
+DELETE FROM Trails
+WHERE trail_id = :trail_id;
 
--- Brain Implant Directory-------------------------------------
+--------------- Brain Implant Directory ---------------
+
 -- Read
-Select implant_id, patron_id, expiration_date, berserk_mode
-From BrainImplants;
+SELECT implant_id, patron_id, expiration_date, berserk_mode
+FROM BrainImplants;
 
 -- Create
-Insert Into BrainImplants (patron_id, expiration_date, berserk_mode)
-    Values (:patron_id, :expiration_date, :berserk_mode);
+INSERT INTO BrainImplants (patron_id, expiration_date, berserk_mode)
+VALUES (:patron_id, :expiration_date, :berserk_mode);
 
 -- Update
-Update BrainImplants
-Set expiration_date = :expiration_date, 
+UPDATE BrainImplants
+SET expiration_date = :expiration_date, 
     berserk_mode = :berserk_mode
-    Where implant_id = :implant_id;
+WHERE implant_id = :implant_id;
 
 -- Delete
-Delete from BrainImplants
-    Where implant_id = :implant_id;
+DELETE FROM BrainImplants
+WHERE implant_id = :implant_id;
 
--- Patron Park Directory---------------------------------------
+--------------- Patron Park Directory ---------------
+
 -- Read
-Select (Select name from Patron), (Select name from Parks), visit_count
-From PatronParks;
+SELECT 
+    (SELECT name FROM Patrons WHERE patron_id = PatronParks.patron_id) AS patron_name, 
+    (SELECT name FROM Parks WHERE park_id = PatronParks.park_id) AS park_name, 
+    visit_count
+FROM PatronParks;
 
 -- Create
-Insert Into PatronParks (patron_id, park_id, visit_count) 
-    Values (
-        (Select patron_id From Patrons Where name = :patronName),
-        (Select park_id From Parks Where name = :parkName),
-        :visit_count
-    );
+INSERT INTO PatronParks (patron_id, park_id, visit_count)
+VALUES (
+    (SELECT patron_id FROM Patrons WHERE name = :patronName), 
+    (SELECT park_id FROM Parks WHERE name = :parkName), 
+    :visit_count
+);
 
 -- Update
-Update PatronParks
-Set visit_count = :visit_count, 
-    Where patron_id = (Select patron_id From Patrons Where name = :patronName)
-    And park_id = (Select park_id From Parks Where name = :parkName)
+UPDATE PatronParks
+SET visit_count = :visit_count
+WHERE patron_id = (SELECT patron_id FROM Patrons WHERE name = :patronName)
+AND park_id = (SELECT park_id FROM Parks WHERE name = :parkName);
 
 -- Delete
-Delete From PatronParks
-    Where patron_id = (Select patron_id From Patrons Where name = :patronName)
-    And park_id = (Select park_id From Parks Where name = :parkName)
+DELETE FROM PatronParks
+WHERE patron_id = (SELECT patron_id FROM Patrons WHERE name = :patronName)
+AND park_id = (SELECT park_id FROM Parks WHERE name = :parkName);
 
--- Patron Trail Directory--------------------------------------
+--------------- Patron Trail Directory ---------------
+
 -- Read
-Select (Select name from Patron), (Select name from Trails), hike_count
-From PatronTrails;
+SELECT 
+    (SELECT name FROM Patrons WHERE patron_id = PatronTrails.patron_id) AS patron_name, 
+    (SELECT name FROM Trails WHERE trail_id = PatronTrails.trail_id) AS trail_name, 
+    hike_count
+FROM PatronTrails;
 
 -- Create
-Insert Into PatronTrails (patron_id, trail_id, hike_count) 
-    Values (
-        (Select patron_id From Patrons Where name = :patronName),
-        (Select trail_id From Trails Where name = :trailName),
-        :hike_count
-    );
+INSERT INTO PatronTrails (patron_id, trail_id, hike_count)
+VALUES (
+    (SELECT patron_id FROM Patrons WHERE name = :patronName), 
+    (SELECT trail_id FROM Trails WHERE name = :trailName), 
+    :hike_count
+);
 
 -- Update
-Update PatronTrails
-Set hike_count = :hike_count, 
-    Where patron_id = (Select patron_id From Patrons Where name = :patronName)
-    And trail_id = (Select trail_id From Trails Where name = :trailName)
+UPDATE PatronTrails
+SET hike_count = :hike_count
+WHERE patron_id = (SELECT patron_id FROM Patrons WHERE name = :patronName)
+AND trail_id = (SELECT trail_id FROM Trails WHERE name = :trailName);
 
 -- Delete
-Delete From PatronTrails
-    Where patron_id = (Select patron_id From Patrons Where name = :patronName)
-    And trail_id = (Select trail_id From Trails Where name = :trailName)
-    
+DELETE FROM PatronTrails
+WHERE patron_id = (SELECT patron_id FROM Patrons WHERE name = :patronName)
+AND trail_id = (SELECT trail_id FROM Trails WHERE name = :trailName);
