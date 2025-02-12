d93a6d5460a44ee034a116215d981a75d9b93f7b
Kelly Shields <shielkel@oregonstate.edu>
2025-02-11 17:14:42 -0800

    added form titles, placeholders to all inputs, and updated CSS

--- SQL/data_manipulation.sql
+++ SQL/data_manipulation.sql
@@ -1,62 +1,63 @@
 
-
-
+-- All queries use : character to 
+-- denote the variables that will have data from the backend programming language
 
 --------------- Patron Directory ---------------
 
--- Read
+
+-- Read - view all patrons
 SELECT patron_id, name, date_of_birth, address
 FROM Patrons;
 
--- Create
+-- Create a new patron
 INSERT INTO Patrons (name, date_of_birth, address)
 VALUES (:name, :date_of_birth, :address);
 
--- Update
+-- Update Patron information
 UPDATE Patrons
 SET name = :name, 
     date_of_birth = :date_of_birth, 
     address = :address
 WHERE patron_id = :patron_id;
 
--- Delete
+-- Delete patron
 DELETE FROM Patrons
 WHERE patron_id = :patron_id;
 
 --------------- Rewards Directory ---------------
 
--- Read
+-- Read - view all reward accounts
 SELECT reward_id, patron_id, reward
 FROM RewardPoints;
 
--- Create
+-- Create a new rewards account for a patron
 INSERT INTO RewardPoints (name, distance, reward)
 VALUES (
     (SELECT name FROM Patrons WHERE name = :name), 
     :reward
 );
 
--- Update
+-- Update patron's reward value
 UPDATE RewardPoints
 SET name = (SELECT name FROM Patrons WHERE name = :name), 
     reward = :reward
 WHERE reward_id = :reward_id;
 
--- Delete
+-- Delete patron's reward account
 DELETE FROM RewardPoints
 WHERE reward_id = :reward_id;
 
 --------------- Park Directory ---------------
 
--- Read
+-- Read - view all parks
 SELECT park_id, name, state, county, has_ranger_station
 FROM Parks;
 
--- Create
+-- Create a new park
 INSERT INTO Parks (name, state, county, has_ranger_station)
 VALUES (:name, :state, :county, :has_ranger_station);
 
--- Update
+-- Update park information
 UPDATE Parks
 SET name = :name, 
     state = :state, 
@@ -64,17 +65,17 @@ SET name = :name,
     has_ranger_station = :has_ranger_station
 WHERE park_id = :park_id;
 
--- Delete
+-- Delete a park
 DELETE FROM Parks
 WHERE park_id = :park_id;
 
 --------------- Trail Directory ---------------
 
--- Read
+-- Read - view all trails
 SELECT trail_id, park_id, name, latitude, longitude, length
 FROM Trails;
 
--- Create
+-- Create a trail
 INSERT INTO Trails (name, latitude, longitude, length)
 VALUES (
     (SELECT name FROM Parks WHERE name = :name), 
@@ -83,7 +84,7 @@ VALUES (
     :length
 );
 
--- Update
+-- Update trail info
 UPDATE Trails
 SET name = (SELECT name FROM Parks WHERE name = :name), 
     latitude = :latitude, 
@@ -91,40 +92,40 @@ SET name = (SELECT name FROM Parks WHERE name = :name),
     length = :length
 WHERE trail_id = :trail_id;
 
--- Delete
+-- Delete a trail
 DELETE FROM Trails
 WHERE trail_id = :trail_id;
 
 --------------- Brain Implant Directory ---------------
 
--- Read
+-- Read - view all implants
 SELECT implant_id, patron_id, expiration_date, berserk_mode
 FROM BrainImplants;
 
--- Create
+-- Create a new implant record for a patron
 INSERT INTO BrainImplants (patron_id, expiration_date, berserk_mode)
 VALUES (:patron_id, :expiration_date, :berserk_mode);
 
--- Update
+-- Update implant record
 UPDATE BrainImplants
 SET expiration_date = :expiration_date, 
     berserk_mode = :berserk_mode
 WHERE implant_id = :implant_id;
 
--- Delete
+-- Delete implant record
 DELETE FROM BrainImplants
 WHERE implant_id = :implant_id;
 
 --------------- Patron Park Directory ---------------
 
--- Read
+-- Read - view all parks a patron has visited
 SELECT 
     (SELECT name FROM Patrons WHERE patron_id = PatronParks.patron_id) AS patron_name, 
     (SELECT name FROM Parks WHERE park_id = PatronParks.park_id) AS park_name, 
     visit_count
 FROM PatronParks;
 
--- Create
+-- Create a new patron park visit
 INSERT INTO PatronParks (patron_id, park_id, visit_count)
 VALUES (
     (SELECT patron_id FROM Patrons WHERE name = :patronName), 
@@ -132,27 +133,27 @@ VALUES (
     :visit_count
 );
 
--- Update
+-- Update a patron park visit
 UPDATE PatronParks
 SET visit_count = :visit_count
 WHERE patron_id = (SELECT patron_id FROM Patrons WHERE name = :patronName)
 AND park_id = (SELECT park_id FROM Parks WHERE name = :parkName);
 
--- Delete
+-- Delete a patron park visit
 DELETE FROM PatronParks
 WHERE patron_id = (SELECT patron_id FROM Patrons WHERE name = :patronName)
 AND park_id = (SELECT park_id FROM Parks WHERE name = :parkName);
 
 --------------- Patron Trail Directory ---------------
 
--- Read
+-- Read - view all trails a patron has visited
 SELECT 
     (SELECT name FROM Patrons WHERE patron_id = PatronTrails.patron_id) AS patron_name, 
     (SELECT name FROM Trails WHERE trail_id = PatronTrails.trail_id) AS trail_name, 
     hike_count
 FROM PatronTrails;
 
--- Create
+-- Create a patron trail visit
 INSERT INTO PatronTrails (patron_id, trail_id, hike_count)
 VALUES (
     (SELECT patron_id FROM Patrons WHERE name = :patronName), 
@@ -160,13 +161,13 @@ VALUES (
     :hike_count
 );
 
--- Update
+-- Update a patron trail visit
 UPDATE PatronTrails
 SET hike_count = :hike_count
 WHERE patron_id = (SELECT patron_id FROM Patrons WHERE name = :patronName)
 AND trail_id = (SELECT trail_id FROM Trails WHERE name = :trailName);
 
--- Delete
+-- Delete a patron trail visit
 DELETE FROM PatronTrails
 WHERE patron_id = (SELECT patron_id FROM Patrons WHERE name = :patronName)
 AND trail_id = (SELECT trail_id FROM Trails WHERE name = :trailName);
