c751a5f7ae78298bbe61e94faebe987bef0f11d4
AustinOregonState <146279644+AustinJHolcomb@users.noreply.github.com>
2025-02-15 10:30:20 -0600

    Added functional drop downs

--- routes/patron_trails.js
+++ routes/patron_trails.js
@@ -8,13 +8,34 @@ router.get("/", (req, res) => {
   let select_table_query =
     "SELECT (SELECT name FROM  Patrons WHERE Patrons.patron_id = PatronTrails.patron_id) AS patron_name, (SELECT name FROM Trails WHERE Trails.trail_id = PatronTrails.trail_id) AS trail_name,  hike_count  FROM PatronTrails;";
 
+  let select_patrons_query =
+    "SELECT patron_id, (SELECT name FROM Patrons WHERE Patrons.patron_id = PatronTrails.patron_id) AS patron_name FROM PatronTrails"
+
+  let select_trails_query =
+    "SELECT trail_id, (SELECT name FROM Trails WHERE Trails.trail_id = PatronTrails.trail_id) AS trail_name FROM PatronTrails"
+
   // Execute the query
   db.pool.query(select_table_query, function (error, rows, fields) {
-    // Render the patron_trails.hbs file, and also send the renderer
-    // an object where 'data' is equal to the 'rows' we
-    // received back from the query
 
-    res.render("patron_trails", { data: rows });
+    //  Save table query
+    data_table = rows;
+
+    db.pool.query(select_patrons_query, function(error, rows, fields) {
+
+      // Save patrons query
+      data_patrons = rows;
+
+      db.pool.query(select_trails_query, function (error, rows, fields) {
+        // Save parks query
+        data_trails = rows;
+
+        // Render the patron_trails.hbs file, and also send the renderer
+        // an object where 'data' is equal to the 'rows' we
+        // received back from the query
+
+        res.render("patron_trails", { patron_trails: data_table, patrons: data_patrons, trails: data_trails });
+      });
+    });
   });
 });
 
--- routes/rewards_points.js
+++ routes/rewards_points.js
@@ -8,14 +8,28 @@ router.get("/", (req, res) => {
   let select_table_query =
     "SELECT reward_id, (SELECT name FROM Patrons WHERE Patrons.patron_id = RewardsPoints.patron_id) AS patron_name, reward FROM RewardsPoints;";
 
-  // Execute the query
+  let select_patrons_query =
+    "SELECT patron_id, (SELECT name FROM Patrons WHERE Patrons.patron_id = RewardsPoints.patron_id) AS patron_name, FROM RewardsPoints"
+  // Execute the query for table
   db.pool.query(select_table_query, function (error, rows, fields) {
-    // Render the rewards_points.hbs file, and also send the renderer
-    // an object where 'data' is equal to the 'rows' we
-    // received back from the query
 
-    res.render("rewards_points", { data: rows });
+    // Save table query
+    let  data_table = rows;
+
+    // Execture  query  for patrons
+    db.pool.query(select_patrons_query, function (error, patrons, fields) {
+
+      // Save patrons
+      let data_patrons  = rows;
+
+      // Render the rewards_points.hbs file, and also send the renderer
+      // an object where 'data' is equal to the 'rows' we
+      // received back from the query
+
+      res.render("rewards_points", { rewards_points: data_table, patrons: data_patrons });
+    });
   });
 });
+
 
 module.exports = router;
